71c2e379e17a732bd2de127c3ee6adbe8bf7fa92
FreytagDom <dominicfreytag@web.de>
2023-03-20 10:01:05 +0100

    add structure

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="preconnect" href="<https://app.snipcart.com>" />
+        <link rel="preconnect" href="<https://cdn.snipcart.com>" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
+        <script
+          async
+          src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"
+        ></script>
+        <div
+          hidden
+          id="snipcart"
+          data-api-key="MzZiYjIyMDQtMWFiNy00MmUyLWJjYzktMzM5YmNhMTk0NDc1NjM4MTQzMTA1OTQ0OTQ0Nzgz"
+          data-config-modal-style="side"
+        ></div>
       </body>
     </Html>
-  )
+  );
 }
